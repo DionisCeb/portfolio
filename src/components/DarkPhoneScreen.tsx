664956bd730c5a0717c1c25142bbd37dcbf65128
DionisCebanu <dionis.cebanu003@gmail.com>
2025-05-14 20:01:28 -0400

    style the component

--- src/components/DarkPhoneScreen.tsx
+++ src/components/DarkPhoneScreen.tsx
@@ -3,19 +3,24 @@ import React from 'react';
 const DarkPhoneScreen: React.FC<{ children?: React.ReactNode }> = ({ children }) => {
     return (
       <div className="flex justify-center items-center h-screen bg-gradient-to-b px-4">
-        <div className="w-[90%] xs:w-[90%] xs1:w-[40%] xl2:w-[50%] aspect-[9/19] rounded-[2rem] bg-black border-[3px] border-gray-800 shadow-2xl relative overflow-hidden p-6 text-white">
-          {/* Camera Dot */}
-          <div className="absolute top-2 left-1/2 -translate-x-1/2 w-2 h-2 bg-gray-700 rounded-full z-10"></div>
+        <div className="w-[90%] xs:w-[90%] xs1:w-[40%] xl2:w-[50%] aspect-[9/19] rounded-[2rem] bg-black border-[3px] border-gray-800 shadow-2xl relative p-6 text-white">
+            {/* Camera Dot */}
+            <div className="absolute top-2 left-1/2 -translate-x-1/2 w-2 h-2 bg-gray-700 rounded-full z-10"></div>
           
-          {/* Side Buttons */}
-          <div className="absolute left-[-6px] top-1/4 w-1 h-10 bg-gray-800 rounded-r-md"></div>
-          <div className="absolute left-[-6px] top-1/2 w-1 h-6 bg-gray-800 rounded-r-md"></div>
-          <div className="absolute right-[-6px] top-1/3 w-1 h-16 bg-gray-800 rounded-l-md"></div>
+            {/* Side Buttons */}
+            <div className="absolute left-[-6px] top-1/4 w-1 h-10 bg-gray-800 rounded-r-md"></div>
+            <div className="absolute left-[-6px] top-1/2 w-1 h-6 bg-gray-800 rounded-r-md"></div>
+            <div className="absolute right-[-6px] top-1/3 w-1 h-16 bg-gray-800 rounded-l-md"></div>
+
+            {/* Side Buttons (Volume & Power) */}
+            <div className="absolute left-[-6px] top-[25%] w-1 h-[8%] bg-gray-800 rounded-r-md" id='volume'></div>
+            <div className="absolute left-[-6px] top-[50%] w-1 h-[6%] bg-gray-800 rounded-r-md"></div>
+            <div className="absolute right-[-6px] top-[35%] w-1 h-[12%] bg-gray-800 rounded-l-md"></div>
   
-          {/* Content Area */}
-          <div className="relative z-0 h-full overflow-y-auto custom-scrollbar">
-            {children}
-          </div>
+            {/* Content Area */}
+            <div className="relative z-0 h-full overflow-y-auto custom-scrollbar">
+                {children}
+            </div>
         </div>
       </div>
     );
